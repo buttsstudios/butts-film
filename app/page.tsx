fcc3c63803dc6b1cfa0e5a7a158877d89834c23b
buttsstudios <buttsstudios@gmail.com>
2026-03-04 16:41:17 -0500

    Add app/page.tsx - Project HACK starter

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'system-ui' }}>
+      <h1>Butts Film</h1>
+      <p>Film and video production portfolio</p>
+      <p style={{ color: '#666', marginTop: '2rem' }}>
+        Deployed via Project HACK
+      </p>
+    </main>
+  );
+}
